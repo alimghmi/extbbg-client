71e57685fb0368d7e399f4cbb94508373ee60498
Ali Moghimi <mghmiali@gmail.com>
2023-05-08 16:31:51 +0000

    apply isort and black

--- app/pcs/client.py
+++ app/pcs/client.py
@@ -1,5 +1,7 @@
 import datetime
+
 from app import client
+from db import mssql
 
 
 class Client(client.Client):
@@ -11,8 +13,8 @@ class Client(client.Client):
         payload = {
             "@type": "DataRequest",
             "identifier": None,
-            "title": self.config['app_name'],
-            "description": self.config['description'],
+            "title": self.config["app_name"],
+            "description": self.config["description"],
             "universe": universe,
             "fieldList": field,
             "trigger": trigger,
@@ -45,60 +47,67 @@ class Client(client.Client):
 
     def _add_new_columns(self):
         self.dataframe["crncy"] = None
-        self.dataframe["timestamp_created_utc"] = datetime.datetime.utcnow()
 
     def _remove_unnecessary_columns(self):
         columns = [
-            'IDENTIFIER',
-            'RC',
-            'LAST_TRADE_TIME',
-            'LAST_TRADE_DATE',
-            'LAST_UPDATE',
-            'LAST_UPDATE_DT',
-            'PX_OPEN',
-            'PX_HIGH',
-            'PX_LOW',
-            'PX_LAST',
-            'PX_BID',
-            'PX_ASK',
-            'PX_VOLUME',
-            'crncy',
-            'HIGH_52WEEK',
-            'LOW_52WEEK'
+            "IDENTIFIER",
+            "RC",
+            "LAST_TRADE_TIME",
+            "LAST_TRADE_DATE",
+            "LAST_UPDATE",
+            "LAST_UPDATE_DT",
+            "PX_OPEN",
+            "PX_HIGH",
+            "PX_LOW",
+            "PX_LAST",
+            "PX_BID",
+            "PX_ASK",
+            "PX_VOLUME",
+            "crncy",
+            "HIGH_52WEEK",
+            "LOW_52WEEK",
         ]
 
         for c in self.dataframe.columns:
             if c not in columns:
                 self.dataframe = self.dataframe.drop(columns=[c])
-        
+
         self.dataframe = self.dataframe[columns]
 
     def _reformat_columns(self):
         get_priority = lambda x: (self.priority[x] if x in self.priority else None)
-        self.dataframe["LAST_UPDATE"] = self.dataframe.apply(self._reformat_last_update, axis=1
+        self.dataframe["LAST_UPDATE"] = self.dataframe.apply(
+            self._reformat_last_update, axis=1
         )
-        self.dataframe["lastTrade"] = (
+        self.dataframe["LAST_TRADE"] = (
             self.dataframe["LAST_TRADE_DATE"] + " " + self.dataframe["LAST_TRADE_TIME"]
         )
-        self.dataframe['timestamp_read_utc'] = self.dataframe.apply(self.to_date, axis=1)
+        self.dataframe["timestamp_read_utc"] = self.dataframe.apply(
+            self.to_date, axis=1
+        )
+        self.dataframe["timestamp_created_utc"] = datetime.datetime.utcnow()
         self.dataframe["priority"] = self.dataframe["IDENTIFIER"].apply(get_priority)
-        del self.dataframe["lastTrade"]
+        del self.dataframe["LAST_TRADE"]
 
     def _save_dataframe_to_database(self):
-        table = self.config['output_table']
+        table = self.config["output_table"]
         self.log.info(f"Inserting data to {table}")
-        self.dataframe.to_csv(f'{table}.sample.csv')
+        conn = mssql.MSSQLDatabase()
+        conn.insert_table(self.dataframe, table)
+        # self.dataframe.to_csv(f'{table}.sample.csv')
 
     def generate_identifier_values(self, tickers):
         result = []
-        template = self.create_identifier_template('ISIN' if self.config['is_identifier_isin'] else 'TICKER')
+        template = self.create_identifier_template(
+            "ISIN" if self.config["is_identifier_isin"] else "TICKER"
+        )
 
         for key, value in tickers.items():
-            tickers_list = value.replace(' ', '').split(',')
+            tickers_list = value.replace(" ", "").split(",")
             for index, ticker in enumerate(tickers_list):
-                identifier_value = f'{key}@{ticker}'
+                identifier_value = f"{key}@{ticker}"
                 identifier = template.copy()
-                identifier['identifierValue'] = identifier_value
+                identifier["identifierValue"] = identifier_value
                 self.priority[identifier_value] = index + 1
                 result.append(identifier)
 
@@ -118,29 +127,31 @@ class Client(client.Client):
 
     @staticmethod
     def to_date(row):
-        x, y = row['LAST_UPDATE'], row['lastTrade']
+        x, y = row["LAST_UPDATE"], row["LAST_TRADE"]
         date_str = x if isinstance(x, str) else y if isinstance(y, str) else None
 
         if date_str is None:
             return None
 
-        date_str = date_str.replace('T', ' ')
-        date = Client._parse_date(date_str, ["%Y-%m-%d %H:%M:%S.%f", "%Y-%m-%d %H:%M:%S"])
+        date_str = date_str.replace("T", " ")
+        date = Client._parse_date(
+            date_str, ["%Y-%m-%d %H:%M:%S.%f", "%Y-%m-%d %H:%M:%S"]
+        )
 
         return date.strftime("%Y-%m-%d %H:%M:%S") if date else None
 
     @staticmethod
     def _reformat_last_update(row):
-        x, y = row['LAST_UPDATE'], row['LAST_UPDATE_DT']
+        x, y = row["LAST_UPDATE"], row["LAST_UPDATE_DT"]
 
         if not isinstance(x, str) and not isinstance(y, str):
             return None
 
-        if ':' not in x:
+        if ":" not in x:
             return Client._parse_date(x, ["%Y%m%d"])
         else:
             date = Client._parse_date(x, ["%Y-%m-%d %H:%M:%S"])
             if not date:
-                date = Client._parse_date(f'{y} {x}', ["%Y-%m-%d %H:%M:%S"])
+                date = Client._parse_date(f"{y} {x}", ["%Y-%m-%d %H:%M:%S"])
 
         return date.strftime("%Y-%m-%d %H:%M:%S") if date else None
