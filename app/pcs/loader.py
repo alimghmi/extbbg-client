eff9a0119b4a0675c49b2208fefb05303c2e7716
Ali Moghimi <mghmiali@gmail.com>
2023-05-08 12:51:44 +0000

    add PCS ticker class

--- app/pcs/loader.py
+++ app/pcs/loader.py
@@ -1,8 +1,10 @@
-from app.loader import Tickers
+from app import loader
 
 
-class PCSTickers(Tickers):
-    
+class Tickers(loader.Tickers):
+    def __init__(self, table_name, columns=None):
+        super().__init__(table_name, columns)
+
     def parse(self):
-        records = self.df.to_dict('records')
-        self.parsed = {row.values[0]: row.values[1] for row in records}
+        records = self.df.to_dict("records")
+        self.parsed = {list(row.values())[0]: list(row.values())[1] for row in records}
