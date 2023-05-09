37e3b6a9bec5a80e99efc7b248a422fdfd5f6fa9
Ali Moghimi <mghmiali@gmail.com>
2023-05-09 15:51:57 +0000

    add where argument to pcs loader

--- app/pcs/loader.py
+++ app/pcs/loader.py
@@ -2,8 +2,8 @@ from app import loader
 
 
 class Tickers(loader.Tickers):
-    def __init__(self, table_name, columns=None):
-        super().__init__(table_name, columns)
+    def __init__(self, table_name, columns=None, where=None):
+        super().__init__(table_name, columns, where)
 
     def parse(self):
         records = self.df.to_dict("records")
